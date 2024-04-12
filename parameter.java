130600bc8707990b6269d663d9b8eb67f86d6039
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2024-04-12 18:44:36 +0530

    Add files via upload

---
+++ parameter.java
@@ -0,0 +1,15 @@
+//PASSING PARAMETERS
+//PARAMETERIZED CONSRUCTOR
+
+import java.util.Scanner;
+class parameter
+{
+parameter(int n)
+{
+System.out.println(n);
+}
+public static void main(String args[])
+{
+parameter t=new parameter(3);
+}
+}
